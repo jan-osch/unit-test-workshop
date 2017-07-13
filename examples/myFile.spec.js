ed1d6f81e478854d590f7d49487342034da01b0d
Janusz Grzesik <janusz.grzesik@vml.com>
2017-07-13 14:27:48 +0200

    Adds sample tests

--- examples/myFile.spec.js
+++ examples/myFile.spec.js
@@ -7,5 +7,9 @@ describe('myFile', () => {
             const expected = 2 * 2 * Math.PI
             expect(circleArea(2)).to.equal(expected)
         })
+
+        it('should be a function', () => {
+            expect(typeof circleArea).to.equal('function');
+        })
     })
 })
